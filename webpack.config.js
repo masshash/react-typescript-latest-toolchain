ad8f408551270dfb9d232fb567b578f8b630909a
Masashi Takahashi <42906988+masshash@users.noreply.github.com>
2021-09-18 19:03:18 +0900

    Add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+          {
+            loader: 'ts-loader',
+          },
+        ],
+      },
+    ],
+  },
+};
