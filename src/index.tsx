896567e6cf3c5cafeedb369a3f415fe2dd3b8ccf
Masashi Takahashi <42906988+masshash@users.noreply.github.com>
2021-09-23 03:48:16 +0900

    Add a little react code to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,7 @@
-// This file is empty. Please edit each one.
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1 style={{ textAlign: 'center' }}>Hello New Tamplate!!</h1>,
+  document.getElementById('root'),
+);
